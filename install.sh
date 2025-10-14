8d440039a33313986b57c709a3fb0f36c74e6201
blackHoleQp <undertalesyt@gmail.com>
2025-10-14 16:10:31 +0000

    Add files via upload

--- install.sh
+++ install.sh
@@ -6,35 +6,26 @@ set -eu
 # Create necessary directories
 mkdir -p ~/.config ~/Pictures ~/.local/share/rofi/themes
 
+sudo pacman -S git wget firefox rofi hyprland waybar kitty kate sddm nwg-look gnome-themes-extra papirus-icon-theme
+
+sudo systemctl enable sddm
+
+cp -r hypr ~/.config
+cp -r waybar ~/.config
+cp -r kitty ~/.config
+
+git clone https://aur.archlinux.org/paru.git
+cd paru
+makepkg -si
+
+paru -Syu
+paru -Suy
+paru -Su
+paru -Sy
+
+paru -S mpvpapaer flclash
+
+
 # Download wallpaper
 cd ~/Pictures
-wget -O gray_bg.html "https://livewallpapers4free.com/download/19154/" || {
-    echo "Failed to download wallpaper, continuing..."
-}
-
-# Install packages
-sudo pacman -S --needed --noconfirm zsh wget hyprland waybar kitty rofi nemo
-
-# Copy configuration files
-cp -r {kitty,hypr,waybar} ~/.config/ 2>/dev/null || echo "Some config folders not found, continuing..."
-cp -r .zshrc ~/ 2>/dev/null || echo ".zshrc not found, continuing..."
-
-# Install Oh My Zsh (if not already installed)
-if [ ! -d "$HOME/.oh-my-zsh" ]; then
-    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
-fi
-
-# Install zsh syntax highlighting
-ZSH_CUSTOM="${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}"
-mkdir -p "$ZSH_CUSTOM/plugins"
-if [ ! -d "$ZSH_CUSTOM/plugins/zsh-syntax-highlighting" ]; then
-    git clone https://github.com/zsh-users/zsh-syntax-highlighting.git "$ZSH_CUSTOM/plugins/zsh-syntax-highlighting"
-fi
-
-# Set zsh as default shell
-if [ "$SHELL" != "$(which zsh)" ]; then
-    chsh -s "$(which zsh)"
-    echo "Zsh set as default shell. Please log out and log back in."
-fi
-
-echo "Installation completed!"
+wget -O gray_bg.html "https://livewallpapers4free.com/download/19154/"
